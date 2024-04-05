ede4d4bdde2f3adc685b186839ad4e444df5a6c2
ankii2001 <ankitluthra2001@gmail.com>
2024-04-05 15:22:16 +0530

    getting things done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -44,18 +44,6 @@ const updateTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
-// const editTask = asyncWrapper(async (req, res) => {
-//   const { id: taskID } = req.params;
-//   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-//     new: true,
-//     runValidators: true,
-//     overwrite: true,
-//   });
-//   if (!task) {
-//    return next(createCustomError(`No task with id: ${taskID}`, 404));
-//   }
-//   res.status(200).json({ task });
-// });
 
 module.exports = {
   getAllTasks,
