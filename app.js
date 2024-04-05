8b3ec8bb90a5bb0b07da9b11b0db3151b956665f
ankii2001 <ankitluthra2001@gmail.com>
2024-04-05 15:15:25 +0530

    added baseURL

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
-const errorHandlerMiddlerware = require("./middleware/error-handler");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
-app.use(errorHandlerMiddlerware);
-// app.get("/hello", (req, res) => {
-//   res.send("<br><hr><h1>Task Manager App</h1><hr>");
-// });
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
+const baseURL = process.env.BASE_URL || `http://localhost:${port}`;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`server is listening on port ${port}...`));
+    app.listen(port, () => {
+      console.log(`Server is listening on ${baseURL}`);
+    });
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
